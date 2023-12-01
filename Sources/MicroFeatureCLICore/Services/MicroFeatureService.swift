956d240c0c44e4972552213bdda1e631bae8edbd
jihoonahn <jihoonahn.dev@gmail.com>
2023-12-01 12:37:04 +0900

    Add MicroFeature Service Feature

--- Sources/MicroFeatureCLICore/Services/MicroFeatureService.swift
+++ Sources/MicroFeatureCLICore/Services/MicroFeatureService.swift
@@ -3,11 +3,14 @@ import Command
 
 /// A service that management MicroFeature Archiecture.
 public final class MicroFeatureService {
-    
+
+    @Command(\.tuist) var tuist
     public init() {}
-    
+
     /// The entry point of the service.
     public func run(path: String, name: String, type: [String]) throws {
-        
+        type.forEach {
+            tuist.scaffold($0, ["--path", path, "--name", name])
+        }
     }
 }
