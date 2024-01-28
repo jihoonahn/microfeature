7eeb29dd35ac3bc6b1a6a66b9622e57f65b31765
jihoonahn <jihoonahn.dev@gmail.com>
2024-01-28 21:34:25 +0900

    Update Version

--- Sources/MicrofeatureCLICore/Constraints/Constants.swift
+++ Sources/MicrofeatureCLICore/Constraints/Constants.swift
@@ -3,5 +3,5 @@ import Foundation
 /// A set of constant values that can be reused across `MicroFeaturePlugin` module.
 enum Constants {
     /// The plugin version.
-    static let version = "0.1.0"
+    static let version = "0.1.2"
 }
