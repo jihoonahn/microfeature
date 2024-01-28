06cc60e05ed25601f89c93baf01d768aef891262
jihoonahn <jihoonahn.dev@gmail.com>
2024-01-28 21:34:10 +0900

    Update Package

--- Package.swift
+++ Package.swift
@@ -13,7 +13,8 @@ let package = Package(
     dependencies: [
         .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0"),
         .package(url: "https://github.com/tuist/projectdescription", from: "3.34.0"),
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.2.3"),
+        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/jihoonahn/tuistui", from: "0.1.7")
     ],
     targets: [
         .executableTarget(
@@ -28,6 +29,7 @@ let package = Package(
             name: "Microfeature",
             dependencies: [
                 .product(name: "ProjectDescription", package: "ProjectDescription"),
+                .product(name: "TuistUI", package: "TuistUI")
             ],
             path: "ProjectDescriptionHelpers"
         ),
--- ProjectDescriptionHelpers/Target/TargetConvertable.swift
+++
@@ -1,6 +0,0 @@
-import ProjectDescription
-
-/// Convert Target Type
-public protocol TargetConvertable {
-    func build() -> ProjectDescription.Target
-}
